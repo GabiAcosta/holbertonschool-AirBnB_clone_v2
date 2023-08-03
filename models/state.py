3f0123aac1e476cdafa097d1272f93307f9eae19
Gabriel <acostagabriel1311@gmai.com>
2023-08-03 12:24:15 -0700

    corrected relationship beetween state and city

--- models/state.py
+++ models/state.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
@@ -17,6 +16,7 @@ class State(BaseModel, Base):
     def cities(self):
         """getter attribute cities that returns the list of City instances
         with state_id equals to the current State.id"""
+        from models.city import City
         from models import storage
         list_cities = []
         for city in storage.all(City).values():
