455101df160402420aa2d33a033c42cfd986605f
MarcosPerdomoVaz <6331@holbertonstudents.com>
2023-08-03 21:15:50 -0300

    task 8

--- models/city.py
+++ models/city.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 from models.state import State
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey(State.id), nullable=False)
+    if models.is_type == "db":
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey(State.id), nullable=False)
+        place = relationship('Place', backref='ciries', cascade='delate')
+    else:
+        state_id = ""
+        name = ""
